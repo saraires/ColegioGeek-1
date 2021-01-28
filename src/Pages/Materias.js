82ec41917edecb374f917193c6ef7011448ed005
Laura Osorio <laurisosorio7@hotmail.com>
2021-01-27 21:13:39 -0500

    Componente Student

--- src/Pages/Materias.js
+++ src/Pages/Materias.js
@@ -2,40 +2,21 @@ import axios from "axios";
 import React, { useEffect, useParams, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Quote from "../Components/Quote";
-import Tabla from "../Components/Tabla";
+import Student from "../Components/Student";
+import Table from "../Components/Table";
 
 
 function Materias() {
-  const [estudiante, setEstudiante] = useState([]);
-  const id = 1;
-
-  useEffect(() => {
-    axios.get(`http://localhost:3004/estudiantes/${id}`).then((res) => {
-      setEstudiante(res.data);
-      console.log(res.data);
-    });
-  }, []);
 
   return (
     <Container className="mb-5">
-      <Container className="mt-5 mb-5">
-        <div className="row  justify-content-center">
-          <h1 text-center>
-            {estudiante.nombre} {estudiante.apellido}
-          </h1>
-        </div>
-        <div className="row  justify-content-center">
-          <h2>
-            {estudiante.grado} {estudiante.grupo}
-          </h2>
-        </div>
-      </Container>
+      <Student/>
       <div className="row m-0 d-flex">
         <Col className="mb-2 p-0 col-sm-4 d-flex align-items-center">
           <Quote />
         </Col>
         <Col  className="p-0 col-sm-7 ">
-          <Tabla />
+          <Table />
         </Col>
       </div>
     </Container>
