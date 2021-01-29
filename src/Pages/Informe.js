9df3ce9cd19dc5267b56681e11ff34e00b886bdb
Laura Osorio <laurisosorio7@hotmail.com>
2021-01-28 22:59:07 -0500

    Adelanto pantalla informe

--- src/Pages/Informe.js
+++ src/Pages/Informe.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { PDFViewer, StyleSheet } from '@react-pdf/renderer';
+import PDF from "../Components/Pdf"
+import { Container } from "react-bootstrap";
 
-function Informe() {
-  return (
-   <h1>Informe</h1>
-  );
-}
+ function Informe() {
 
+  const styles = StyleSheet.create({
+    section: {
+      display:"block",
+      margin: "auto",
+      width:"50vw",
+      height:"50vw",
+      flexGrow: 1
+    }
+  });
+
+   return(
+     <Container className="mt-5">
+       <PDFViewer style={styles.section}>
+       <PDF />
+     </PDFViewer>
+     </Container>
+   )
+ }
 export default Informe;
