5827086385e84187ab1879be428692d5eb260e3a
Laura Osorio <laurisosorio7@hotmail.com>
2021-01-31 19:47:47 -0500

    nav notas

--- src/Pages/EstudianteNotas.js
+++ src/Pages/EstudianteNotas.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Student from "../Components/Student"
-import TableNotes from "../Components/TableNotes"
+import React from "react";
+import { Container } from "react-bootstrap";
+import Student from "../Components/Student";
+import TableNotes from "../Components/TableNotes";
 import { useParams } from "react-router-dom";
-import MenuEstudiante from '../Components/menuEstudiante';
-import Encabezado from "../Components/Navbar"
+import MenuEstudiante from "../Components/menuEstudiante";
+
 function Notas() {
+  const id = useParams();
+  console.log(id);
 
-    const id = useParams();
-    console.log(id);
-
-    return (
-        <>
-                <Encabezado/>
-        <Container>
-                {/* <MenuEstudiante /> */}
-                <Student />
-                <TableNotes />
-            </Container>
-</>
-    )
+  return (
+    <>
+      <MenuEstudiante />
+      <Container>
+        {/* <MenuEstudiante /> */}
+        <Student />
+        <TableNotes />
+      </Container>
+    </>
+  );
 }
 
 export default Notas;
