23e090231e52c3917119622b1c4d6c78f9b27359
Laura Osorio <laurisosorio7@hotmail.com>
2021-02-01 16:12:48 -0500

    Intento conexion 1

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import  axios  from 'axios';
 import { Card, Container, Button } from "react-bootstrap";
 import Logo from "../Images/LogoAcademia.png"
 import { Formik, Form, Field } from 'formik';
@@ -7,9 +7,25 @@ import * as Yup from 'yup';
 
 const signInSchema = Yup.object().shape({
   email: Yup.string().email('Correo Invalido').required('Ingrese un correo'),
-  password: Yup.string().required('Ingrese contraseña').min(8, "La contraseña debe tener mínimo 8 caracteres"),
+  password: Yup.string().required('Ingrese contraseña').min(8, "La contraseña debe tener mínimo 8 caracteres").matches(/[a-z]/, 'Debe tener una letra en minúscula')
+  .matches(/[A-Z]/, 'Debe tener una letra en mayuscula'),
   picked: Yup.number(!1 || !2 || !3).required("Escoja un rol"),
 });
+
+// const login = (values) =>{
+//   console.log(values);
+//   axios.post('http://localhost:5000',{
+//     email: values.email,
+//     password: values.password,
+//     rol: values.rol
+//   }).then((res)=>{
+//     if(res.data.message === `Información Incorrecta`){
+//       alert("Correo y/o contraseña incorrectos")
+//     }else {
+//       console.log(res.data);
+//     }
+//   })
+// };
 
 const FormFormik = () => (
   <div>
@@ -21,11 +37,23 @@ const FormFormik = () => (
       }}
       validationSchema={signInSchema}
       onSubmit={async (values) => {
-        // same shape as initial values
-        await new Promise((r) => setTimeout(r, 500));
-        console.log(values);
+        axios.post('http://localhost:5000',{
+          email: values.email,
+          password: values.password,
+          rol: values.rol
+        }).then((res)=>{
+          return res.data
+        })
+        // // same shape as initial values
+        // console.log('Se rellenaron todos los campos')
+        // console.log(values);
+
+        // localStorage.setItem("correo", values.email )
+        // localStorage.setItem("password", values.password )
+        // localStorage.setItem("rol", values.picked )
       }}
     >
+      
       {({ errors, touched, values }) => (
         <Container className="mt-4">
           <Card className="d-flex m-auto my-auto pb-5 shadow-lg" style={{ width: "20rem" }}>
