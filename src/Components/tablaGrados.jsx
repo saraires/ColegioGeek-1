a0a94272d9deb8d0a9a70d5372f15ef044f8b8b0
Laura Osorio <laurisosorio7@hotmail.com>
2021-02-05 14:21:34 -0500

    modal notas

--- src/Components/tablaGrados.jsx
+++ src/Components/tablaGrados.jsx
@@ -1,61 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import {getFromLocal, saveToLocal} from "../functions/localstorage"
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getFromLocal, saveToLocal } from "../functions/localstorage";
+import axios from "axios";
 
 function TablaGrados() {
-const id = getFromLocal("id_usuario");
-const nombre = getFromLocal("nombre_completo");
-    const [profesor, setProfesor] = useState([]);
+  const id = getFromLocal("id_usuario");
+  const nombre = getFromLocal("nombre_completo");
+  const [profesor, setProfesor] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/profesor-grupos/${id}`)
+      .then((res) => {
+        setProfesor(res.data.rows);
+        console.log(res.data.rows);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-        axios.get(`http://localhost:5000/profesor-grupos/${id}`).then((res) => {
-            setProfesor(res.data.rows);
-            console.log(res.data.rows);
-
-        }).catch((err) => {
-            console.log(err);
-        });
-
-    }, []);
-
-    return (
-        <div className="container" style={{ marginTop: "40px" }}>
-            <h3 className="card-tittle text-center">{nombre} </h3>
-            <br />
-            <table className="table table-bordered">
-                <thead>
-                    <tr className="table-info">
-                        <th scope="col">Codigo de grupo</th>
-                        <th scope="col">Grupo</th>
-                        <th scope="col">Jornada</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        profesor.map((item,index)=>{
-                            return(
-                                <tr>
-                                    <td>
-                                        {item.cod_grupo}
-                                        <Link to ={`/profesor-notas/${id}/${item.id_grupo}/${item.cod_grupo}`} style={{ float: "right", color:"#47525E" }}><button type="button" class="btn btn-outline-info" onClick={()=>{saveToLocal("cod_grupo",item.cod_grupo);
-
-                                        saveToLocal("id_grupo", item.id_grupo)
-                                    }}  >Ver notas</button></Link>  
-                                    </td>
-                                    <td>{item.descripcion_grupo}</td>
-                                    <td>{item.jornada}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                    
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="container" style={{ marginTop: "40px" }}>
+      <h3 className="card-tittle text-center">{nombre} </h3>
+      <br />
+      <table className="table table-bordered">
+        <thead>
+          <tr className="table-info">
+            <th scope="col">Codigo de grupo</th>
+            <th scope="col">Grupo</th>
+            <th scope="col">Jornada</th>
+          </tr>
+        </thead>
+        <tbody>
+          {profesor.map((item, index) => {
+            return (
+              <tr>
+                <td>
+                  {item.cod_grupo}
+                  <Link
+                    to={`/profesor-notas/${id}/${item.id_grupo}/${item.cod_grupo}`}
+                    style={{ float: "right", color: "#47525E" }}
+                  >
+                    <button
+                      type="button"
+                      class="btn btn-outline-info"
+                      onClick={() => {
+                        saveToLocal("cod_grupo", item.cod_grupo);
+                        saveToLocal("id_estudiante", item.id_estudiante);
+                        saveToLocal("id_grupo", item.id_grupo);
+                      }}
+                    >
+                      Ver notas
+                    </button>
+                  </Link>
+                </td>
+                <td>{item.descripcion_grupo}</td>
+                <td>{item.jornada}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TablaGrados;
--- src/Components/tablaNotas.jsx
+++ src/Components/tablaNotas.jsx
@@ -1,75 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios'
-import { getFromLocal } from '../functions/localstorage';
-import { Container, Table } from 'react-bootstrap';
-
+import React, { useEffect, useState } from "react";
+// import { Link } from 'react-router-dom';
+import axios from "axios";
+import { getFromLocal } from "../functions/localstorage";
+import { Button, Container, Form, Modal, Table } from "react-bootstrap";
 
 function TablaGrados() {
-const id = getFromLocal("id_usuario");
-const codigo = getFromLocal("cod_grupo");
-const grupo = getFromLocal("id_grupo")
-console.log(id);
-console.log(codigo);
-    const [notas, setNotas] = useState([]);
+  const id = getFromLocal("id_usuario");
+  const codigo = getFromLocal("cod_grupo");
+  const grupo = getFromLocal("id_grupo");
+  const [notas, setNotas] = useState([]);
 
-    useEffect(() => {
+  //modal
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
-        axios.get(`http://localhost:5000/profesor-notas/${id}/${grupo}/${codigo}`).then((res) => {
-            setNotas(res.data.rows);
-            console.log(res.data);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/profesor-notas/${id}/${grupo}/${codigo}`)
+      .then((res) => {
+        setNotas(res.data.rows);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
 
-        }).catch((err) => {
-            console.log(err);
-        });
+  return (
+    <Container>
+      <Table striped hover size="md" className="table-responsive ">
+        <thead className="text-info text-center table-bordered">
+          <tr className="table-info">
+            <th scope="col">Codigo Estudiante</th>
+            <th scope="col">Nombre Estudiante</th>
+            <th scope="col">Seguimiento</th>
+            <th scope="col">Conocimiento</th>
+            <th scope="col">Bimensual</th>
+            <th scope="col">Autoevaluación</th>
+            <th scope="col">Nota Final</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
 
-    }, []);
+        <tbody className=" text-center table-bordered">
+          {notas.map((notas) => {
+            return (
+              <tr key={notas._id}>
+                <td>{notas.cod_estudiante}</td>
+                <td>{notas.nombre_completo}</td>
+                <td>{Number(notas.seguimiento).toFixed(2)}</td>
+                <td>{Number(notas.bimensual_1).toFixed(2)}</td>
+                <td>{Number(notas.bimensual_2).toFixed(2)}</td>
+                <td>{Number(notas.autoevaluacion).toFixed(2)}</td>
+                <td>{Number(notas.nota_promedio).toFixed(2)}</td>
+                <td>
+                  <button className="btn btn-success" onClick={handleShow}>
+                    Editar Notas
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
 
-    return (
-        <Container>
-            <Table striped hover size="md" className="table-responsive ">
-            <thead className="text-info text-center table-bordered">
-                    <tr className="table-info">
-                        <th scope="col">Codigo Estudiante</th>
-                        <th scope="col">Nombre Estudiante</th>
-                        <th scope="col">Seguimiento</th>
-                        <th scope="col">Conocimiento</th>
-                        <th scope="col">Bimensual</th>
-                        <th scope="col">Autoevaluación</th>
-                        <th scope="col">Nota Final</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar Notas</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Seguimiento</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ingrese la nota de seguimiento"
+              />
+            </Form.Group>
 
-                <tbody className=" text-center table-bordered">
-                    {
-                        notas.map((notas) => {
-                            return (
-                                <tr key={notas._id}>
-                                    <td>{notas.cod_estudiante}</td>
-                                    <td>{notas.nombre_completo}
-                                    </td>
-                                    <td>{Number(notas.seguimiento).toFixed(2)}</td>
-                                    <td>{Number(notas.bimensual_1).toFixed(2)}</td>
-                                    <td>{Number(notas.bimensual_2).toFixed(2)}</td>
-                                    <td>{Number(notas.autoevaluacion).toFixed(2)}</td>
-                                    <td>{Number(notas.nota_promedio).toFixed(2)}</td>
-                                    <td>
-                                        <button className="btn btn-success">
-                                            Editar Notas
-                                        </button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-
-                  
-                </tbody>
-
-            </Table>
-        </Container>
-    );
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
 }
 
 export default TablaGrados;
