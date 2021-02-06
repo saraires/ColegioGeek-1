5b7e831e78df22fb93e681500cba3570a1112bad
Sarai Restrepo <sarairestrepo22@gmail.com>
2021-02-06 16:35:25 -0500

    pull

--- src/Components/tablaNotas.jsx
+++ src/Components/tablaNotas.jsx
@@ -4,38 +4,6 @@ import { getFromLocal, saveToLocal } from "../functions/localstorage";
 import { Container, Table } from "react-bootstrap";
 import EditarNotas from "./EditarNotas";
 
-/* NOTAS
-En el boton editar notas: Guardar el codigo de estudiante (preferiblemente id_estudiante) 
-y llamar una funcion en donde estan las const que haga una peticion al backend (En rutas profesor hay una peticion put)
-la cual traiga los datos previos a la edicion
-es decir hacer un select de la tabla de notas de ese estudiante.
-
-los valores de id_materia hay que traerlos desde la tabla de "tabla grados", a traves del get que hay en el back
-(agregue ese campo en el Select que hiciste) y la idea es que al recibir la respuesta, en algun lado del localStorage se guarde
-para reemplazar ese valor en el query y pues si
-
-Ya en el modal hay que hacer que los "values", dentro del modalbody, capturen los datos que hay ahi para que sean visibles por lo que se va a cambiar
-No se si me hago entender, pero pues, a idea es que no aparesca un input vacio para editar sino que si ahi en la nota habia un 3,5 pues que se siga 
-viendo hasta que se edite (Algo como un placehorlder)
-
-Con ese dato de id_materia de traido del getlocal, los dos botones del modal footer pues la idea es que ese "aceptar" haga que se ejecute el put que hay
-en el back con este insert:
-
-(INSERT INTO notas(id_notas, id_materia, id_estudiante, seguimiento, 
-bimensual_1, bimensual_2, autoevaluacion) 
-VALUES(nextval('notas_id_seq'), 1, 1, 2.5, 3.0, 
-5.0, 5.0);)
-
-para actualizar los campos nuevos
-
-y el de cancelar pues solo cierra el modal y no llama al put :T
-
-No se si me hice entender, perdon no haber podido hacer mas :c
-
-
-(lo de las rutas creo que es mas facil enviarlo por params)
- */
-
 function TablaGrados() {
   const id = getFromLocal("id_usuario");
   const codigo = getFromLocal("cod_grupo");
